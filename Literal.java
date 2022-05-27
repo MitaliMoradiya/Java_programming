9ec842e34c54a329cd18ae94fe67b7c20bb58e84
Java_Programming <106155899+MitaliMoradiya@users.noreply.github.com>
2022-05-27 13:18:05 +0530

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,12 @@
+public class Literal
+ {
+    public static void mian(String[] args)
+    {
+        int age = 40;
+        System.out.println (age);
+
+        String str = "mitali";
+        System.out.println(str);
+    }
+    
+ }
---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum{
+    public static void main (String[] args)
+    {
+        int num1 = 20;
+        int num2 = 40;
+        int num3 = 50;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
