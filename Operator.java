1c1153526d4e0537d7c20610505dd5d10cf05b4d
Java_Programming <106155899+MitaliMoradiya@users.noreply.github.com>
2022-05-28 23:01:09 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,20 @@
+public class Operator 
+{
+    public static void main(String[] args) 
+    {
+        int a = 4;
+        //int b = 6 * a;
+        int b = 9;
+
+        b *= 3;
+        System.out.println(b);
+        System.out.println(6<8); //comparison operator
+        System.out.println(8<10 && 18<8); //logical operator AND
+        System.out.println(8<10 || 18<8); // OR
+
+
+
+        
+    }
+    
+}
---
+++ Practise1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Practise1 {
+    public static void main(String[] args)
+
+    {
+        // QUE1:   Sum of Three number
+        //int a=10, b=20, c=30;
+        //int sum = a + b + c;
+        //System.out.println(sum);
+
+
+        
+        //QUE2:  calculate cgpa using marks of three subject
+
+        /*float m1 = 80;
+        float m2 = 50;
+        float m3 = 70;
+
+        float cgpa = (m1 + m2 + m3)/30;
+        System.out.println(cgpa);*/
+
+
+
+        //QUE3: 
+        /*  System.out.println("what's Your Name!!");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        System.out.println("Hello " + name + "have a good day");*/
+
+
+
+
+        //Que4: kilo meter to miles
+        /*Scanner sc = new Scanner(System.in);
+        double kilometer, miles;
+
+        System.out.println("enter kilometer: ");
+        kilometer = sc.nextDouble();
+
+        miles=kilometer/1.609;
+
+        System.out.println("kilometer to miles:" + miles);*/
+
+
+
+
+        //Que5: num is int or not
+        
+       /* System.out.println("enter number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());*/
+
+
+
+
+    }
+    
+}
