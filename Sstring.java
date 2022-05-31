63cc63c30ed54f8768b8861f8a6c71f5be568d54
Java_Programming <106155899+MitaliMoradiya@users.noreply.github.com>
2022-05-31 23:03:16 +0530

    Add files via upload

---
+++ Sstring.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Sstring {
+    public static void main(String[] args) {
+       // String name = new String("mitali");
+         //String name = "mitali";
+        // System.out.println(name);
+
+         //SOME WAYS TO PRINTLINE:
+         /*System.out.print("Hello"); // print without newline
+         System.out.println("Hello");// print with new line
+         System.out.printf("helllo");*/
+
+        /* int a = 6;
+         float b = 10.676f;
+         System.out.printf("the value of a is %d and value of b is %f",a,b);
+                            //%d is format specifier
+         System.out.format("the value of a is %d and value of b is %f",a,b); // same as printf*/
+
+         //TAKE INPUT FROM USER:
+        /* Scanner sc = new Scanner(System.in);
+         System.out.println("enter your name: ");
+         String name = sc.next(); // dont't print after space 
+         //String name = sc. nextLine(); // print whole sentence 
+        System.out.println(name);*/
+
+        char[] str = {'h', 'e', 'l', 'l', 'o'};
+        String s = new String(str);
+        
+         
+         
+    }
+    
+}
+
+
