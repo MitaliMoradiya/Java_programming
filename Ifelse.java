35fade2c848e585556cc6a0b5d279f4e4beebf32
Java_Programming <106155899+MitaliMoradiya@users.noreply.github.com>
2022-06-01 22:39:06 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+class Ifelse {
+
+    public static void main(String[] args) {
+        
+        int age  = 6;
+        if (age > 18)
+        {
+            System.out.println("Yes you can drive");
+
+        }
+        else
+        {
+            System.out.println(No  you cannot drive");
+        }
+    }
+}    
